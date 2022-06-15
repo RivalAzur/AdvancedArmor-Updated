f11468e8f890bc0822d1768fdcd79838ca99c77f
iLucaH <lucahautrive@gmail.com>
2022-06-15 16:38:48 +1000

    UltraPrisonCore autosell fix

--- src/main/java/me/ilucah/advancedarmor/boosting/providers/UPCMoneyProvider.java
+++ src/main/java/me/ilucah/advancedarmor/boosting/providers/UPCMoneyProvider.java
@@ -1,7 +1,9 @@
 package me.ilucah.advancedarmor.boosting.providers;
 
+import dev.drawethree.ultraprisoncore.UltraPrisonCore;
 import dev.drawethree.ultraprisoncore.autosell.api.events.UltraPrisonAutoSellEvent;
 import dev.drawethree.ultraprisoncore.autosell.api.events.UltraPrisonSellAllEvent;
+import dev.drawethree.ultraprisoncore.multipliers.enums.MultiplierType;
 import me.ilucah.advancedarmor.AdvancedArmor;
 import me.ilucah.advancedarmor.armor.BoostType;
 import me.ilucah.advancedarmor.boosting.model.BiBoostProvider;
@@ -21,12 +23,20 @@ public class UPCMoneyProvider extends BiBoostProvider<UltraPrisonAutoSellEvent, 
     @Override
     @EventHandler(priority = EventPriority.LOWEST)
     public void onBoostT(UltraPrisonAutoSellEvent event) {
-        event.setMoneyToDeposit(resolveNewAmount(event.getPlayer(), event.getMoneyToDeposit()));
+        double totalAmount = event.getItemsToSell().values().stream().mapToDouble(Double::doubleValue).sum();
+        if (UltraPrisonCore.getInstance().getAutoSell().isMultipliersModuleEnabled())
+            totalAmount = (long) UltraPrisonCore.getInstance().getMultipliers().getApi().getTotalToDeposit(event.getPlayer(), totalAmount, MultiplierType.SELL);
+        double amountToGive = resolveNewAmount(event.getPlayer(), totalAmount) - totalAmount;
+        UltraPrisonCore.getInstance().getEconomy().depositPlayer(event.getPlayer(), amountToGive);
     }
 
     @Override
     @EventHandler(priority = EventPriority.LOWEST)
     public void onBoostV(UltraPrisonSellAllEvent event) {
-        event.setSellPrice(resolveNewAmount(event.getPlayer(), event.getSellPrice()));
+        double totalAmount = event.getItemsToSell().values().stream().mapToDouble(Double::doubleValue).sum();
+        if (UltraPrisonCore.getInstance().getAutoSell().isMultipliersModuleEnabled())
+            totalAmount = (long) UltraPrisonCore.getInstance().getMultipliers().getApi().getTotalToDeposit(event.getPlayer(), totalAmount, MultiplierType.SELL);
+        double amountToGive = resolveNewAmount(event.getPlayer(), totalAmount) - totalAmount;
+        UltraPrisonCore.getInstance().getEconomy().depositPlayer(event.getPlayer(), amountToGive);
     }
 }
